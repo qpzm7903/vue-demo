d11c2133e7628d4191bf99d35a8a2f665278a67b
qpzm7903 <yx372114301@qq.com>
2022-07-17 22:36:45 +0800

    在todoList里使用v-for创建todoItem,并且传递参数

--- todo_list_demo/todo_list_demo/src/components/TodoItem.vue
+++ todo_list_demo/todo_list_demo/src/components/TodoItem.vue
@@ -1,58 +1,55 @@
 <template>
-    <div>
-        <li>
-          <label>
-            <input type="checkbox"/>
-            <span>xxxxx</span>
-          </label>
-          <button class="btn btn-danger" style="display:none">删除</button>
-        </li>
-    </div>
+  <div>
+    <li>
+      <label>
+        <input type="checkbox" :checked="todo.done" />
+        <span>{{ todo.title }}</span>
+      </label>
+      <button class="btn btn-danger" style="display:none">删除</button>
+    </li>
+  </div>
 </template>
 
 <script>
-    export default {
-        name :"TodoItem"
-    }
+export default {
+  name: "TodoItem",
+  props: ["todo"]
+}
 </script>
 
 <style  scoped>
+/*item*/
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
 
+li label {
+  float: left;
+  cursor: pointer;
+}
 
-  /*item*/
-  li {
-    list-style: none;
-    height: 36px;
-    line-height: 36px;
-    padding: 0 5px;
-    border-bottom: 1px solid #ddd;
-  }
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
 
-  li label {
-    float: left;
-    cursor: pointer;
-  }
+li button {
+  float: right;
+  display: none;
+  margin-top: 3px;
+}
 
-  li label li input {
-    vertical-align: middle;
-    margin-right: 6px;
-    position: relative;
-    top: -1px;
-  }
+li:before {
+  content: initial;
+}
 
-  li button {
-    float: right;
-    display: none;
-    margin-top: 3px;
-  }
-
-  li:before {
-    content: initial;
-  }
-
-  li:last-child {
-    border-bottom: none;
-  }
-
-
+li:last-child {
+  border-bottom: none;
+}
 </style>
--- todo_list_demo/todo_list_demo/src/components/TodoList.vue
+++ todo_list_demo/todo_list_demo/src/components/TodoList.vue
@@ -1,36 +1,43 @@
 <template>
-    <div>
-      <ul class="todo-main">
-
-        <TodoItem/>
-
-      </ul>
-    </div>
+  <div>
+    <ul class="todo-main">
+      <TodoItem v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj" />
+    </ul>
+  </div>
 </template>
 
 <script>
 import TodoItem from './TodoItem.vue';
-    export default {
-    name: "TodoList",
-    components: { TodoItem }
+export default {
+  name: "TodoList",
+  components: { TodoItem },
+  data () {
+    return {
+      todos: [
+        { id: '001', title: "code", done: true },
+        { id: '002', title: "code", done: false },
+        { id: '003', title: "code", done: true },
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
-  /*main*/
-  .todo-main {
-    margin-left: 0px;
-    border: 1px solid #ddd;
-    border-radius: 2px;
-    padding: 0px;
-  }
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
 
-  .todo-empty {
-    height: 40px;
-    line-height: 40px;
-    border: 1px solid #ddd;
-    border-radius: 2px;
-    padding-left: 5px;
-    margin-top: 10px;
-  }
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
 </style>
