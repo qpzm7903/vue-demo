0fafab1b7411bb80c24ac2b3fa17613efa7fa662
qpzm7903 <yx372114301@qq.com>
2022-07-17 11:29:15 +0800

    init todo list vue demo

---
+++ todo_list_demo/todo_list_demo/src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <ul class="todo-main">
+
+        <TodoItem/>
+
+      </ul>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+    export default {
+    name: "TodoList",
+    components: { TodoItem }
+}
+</script>
+
+<style scoped>
+  /*main*/
+  .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+
+  .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
